5e2c9df95a05410721b47dc738fd6a4bd6902de9
Anastron <armin_benz@web.de>
2013-04-29 19:00:54 +0200

    addTimerSR
    mistakes in the sourcecode

---
+++ src/gui/TimerSR.java
@@ -0,0 +1,57 @@
+package gui;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TimerSR extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JButton btnBlueBuff;
+	private JLabel lblStatus;
+	public TimerSR() {
+		initGUI();
+		setLocationRelativeTo(null);
+		
+	}
+	private void initGUI() {
+		setSize(new Dimension(256, 470));
+		setResizable(false);
+		getContentPane().setLayout(null);
+		
+		btnBlueBuff = new JButton("Blue Buff");
+		btnBlueBuff.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setTimerBB();
+			}
+		});
+		btnBlueBuff.setBounds(10, 24, 89, 23);
+		getContentPane().add(btnBlueBuff);
+		
+		lblStatus = new JLabel("Status");
+		lblStatus.setBounds(170, 33, 46, 14);
+		getContentPane().add(lblStatus);
+	}
+
+	public void setTimerBB(){
+		public void setText(final String text){
+			SwingUtilities.invokeLater(new Runnable() {
+		        public void run() {
+		            setText(text);
+		        }
+			});
+		}
+		public void run(){
+			for(int i = 10; i > 0; i--){
+				final String text = "(" + i + ") seconds left";
+				setText();
+			}
+			UsedBefore window2 = UsedBefore();
+			window2.setVisible(true);
+		}
+	}
+}
