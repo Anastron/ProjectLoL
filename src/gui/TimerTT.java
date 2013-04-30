d7809ded3f0c6d2d7f912b0e1756b492a7515d2f
StrSimon <SStreblow@gmx.de>
2013-04-30 20:27:19 +0200

    Junglemonster TT eingefügt

--- src/gui/TimerTT.java
+++ src/gui/TimerTT.java
@@ -1,4 +1,5 @@
 package gui;
+
 // bin noch am überlegen :-)
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -9,39 +10,91 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
-public class TimerTT extends JFrame {
+public class TimerTT<Static> extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private JButton btnBlueBuff;
+	private JButton btnGolemR;
+	private JButton btnGolemL;
+	private JButton btnWolfR;
+	private JButton btnWolfL;
+	private JButton btnWraithR;
+	private JButton btnWraithL;
+	private JButton btnSpider;
 	private JLabel lblStatus;
+
 	public TimerTT() {
 		initGUI();
 		setLocationRelativeTo(null);
-		
+
 	}
+
 	private void initGUI() {
 		setSize(new Dimension(256, 470));
 		setResizable(false);
 		getContentPane().setLayout(null);
-		
-		btnBlueBuff = new JButton("Blue Buff");
-		btnBlueBuff.addActionListener(new ActionListener() {
+
+		btnGolemR= new JButton("Golem rechts");
+		btnGolemR.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-//				setTimerBB();
 			}
-		});
-		btnBlueBuff.setBounds(10, 24, 89, 23);
-		getContentPane().add(btnBlueBuff);
+			});
+		btnGolemR.setBounds(10, 25, 89, 23);
+		getContentPane().add(btnGolemR);
 		
-		lblStatus = new JLabel("Status");
+		btnGolemL = new JButton("Golem links");
+		btnGolemL.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnGolemL.setBounds(10, 50, 89, 23);
+		getContentPane().add(btnGolemL);
+		
+		btnWolfR = new JButton("Wolf rechts");
+		btnWolfR.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnWolfR.setBounds(10, 75, 89, 23);
+		getContentPane().add(btnWolfR);
+		
+		btnWolfL = new JButton("Wolf links");
+		btnWolfL.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnWolfL.setBounds(10, 100, 89, 23);
+		getContentPane().add(btnWolfL);
+		
+		btnWraithR = new JButton("Wraith rechts");
+		btnWraithR.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnWraithR.setBounds(10, 125, 89, 23);
+		getContentPane().add(btnWraithR);
+		
+		btnWraithL = new JButton("Wraith links");
+		btnWraithL.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnWraithL.setBounds(10, 150, 89, 23);
+		getContentPane().add(btnWraithL);
+		
+		btnSpider = new JButton("Spieder");
+		btnSpider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+			});
+		btnSpider.setBounds(10, 175, 89, 23);
+		getContentPane().add(btnSpider);
+
+		lblStatus = new JLabel("kp");
 		lblStatus.setBounds(170, 33, 46, 14);
 		getContentPane().add(lblStatus);
-		
-		
-	
-		
+
 	}
-	 
-	 public class MyTimer extends Thread { //ungetestet
+
+	public class MyTimer extends Thread { //ungetestet
 	     public void setText(final String text){
 	       SwingUtilities.invokeLater(new Runnable() {
 	             public void run() {
